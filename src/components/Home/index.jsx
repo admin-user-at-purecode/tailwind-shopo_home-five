ab6cf1e0e86bbb6b54ba546dc3d46bc998ce014a
unknown <kaushik4759@gmail.com>
2024-03-11 13:17:54 +0530

    Update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -17,12 +17,6 @@ function Index() {
     <Layout childrenClasses=" pt-0">
       <Banner className="mb-[60px]" />
       <ServiceBanner/>
-      <DesignCardsSection
-        products={products.slice(16, 20)}
-        sectionTitle="Trendy Design"
-        seeMoreUrl="/all-products"
-        className="new-products mb-[60px]"
-      />
       <BrandSection className="mb-[60px]" />
       <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
       <DesignCardsSection
