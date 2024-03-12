cd398b502bcd2cda1114fe7810c42ada7208d0bf
unknown <kaushik4759@gmail.com>
2024-03-11 18:31:35 -0700

    Update

--- src/components/SectionStyles/DesignCardsSection.jsx
+++ src/components/SectionStyles/DesignCardsSection.jsx
@@ -12,7 +12,7 @@ export default function DesignCardsSection({
     <div data-aos="fade-up" className={`section-style-one ${className || ""}`}>
       <ViewMoreTitle categoryTitle={sectionTitle} seeMoreUrl={seeMoreUrl}>
         <div className="products-section w-full">
-          <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-[30px] gap-5">
+          <div className="grid xl:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-[30px] gap-5">
             <DataIteration
               datas={products}
               startLength={0}
