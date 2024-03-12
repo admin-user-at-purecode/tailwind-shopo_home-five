b97049a06da4e53fc364b64be21f360ccd482883
unknown <kaushik4759@gmail.com>
2024-03-11 19:31:07 -0700

    Update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -14,47 +14,66 @@ import ServiceBanner from "./ServiceBanner";
 function Index() {
   const { products } = datas;
   return (
-    <Layout childrenClasses=" pt-0">
-      <Banner className="mb-[60px]" />
-      <ServiceBanner/>
-      <DesignCardsSection
-        products={products.slice(16, 20)}
-        sectionTitle="Trendy Design"
-        seeMoreUrl="/all-products"
-        className="new-products mb-[60px]"
-      />
-      <BrandSection className="mb-[60px]" />
-      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
-      <DesignCardsSection
-        products={products.slice(20, 24)}
-        sectionTitle="Feature Design"
-        seeMoreUrl="/all-products"
-        className="new-products mb-[60px]"
-      />
-      <ProductsAds
-        ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-3.png`]}
-        className="products-ads-section mb-[60px]"
-      />
-      <NewArrival
-        sectionTitle="New Arrival"
-        seeMoreUrl="/all-products"
-        products={products.slice(16, 28)}
-        className="mb-[60px]"
-      />
+      <Layout childrenClasses=" pt-0">
+          <Banner className="mb-[60px]" />
+          <ServiceBanner />
+          <DesignCardsSection
+              products={products.slice(16, 20)}
+              sectionTitle="Trendy Design"
+              seeMoreUrl="/all-products"
+              className="new-products mb-[60px]"
+          />
+          <BrandSection className="mb-[60px]" />
+          <CampaignCountDown
+              lastDate="2023-10-04 4:00:00"
+              className="mb-[60px]"
+          />
+          <DesignCardsSection
+              products={products.slice(20, 24)}
+              sectionTitle="Feature Design"
+              seeMoreUrl="/all-products"
+              className="new-products mb-[60px]"
+          />
+          <ProductsAds
+              ads={[
+                  `${
+                      import.meta.env.VITE_PUBLIC_URL
+                  }/src/assets/images/ads-3.png`,
+              ]}
+              className="products-ads-section mb-[60px]"
+          />
+          <ProductsAds
+              ads={[
+                  `${
+                      import.meta.env.VITE_PUBLIC_URL
+                  }/src/assets/images/ads-3.png`,
+              ]}
+              className="products-ads-section mb-[60px]"
+          />
+          <NewArrival
+              sectionTitle="New Arrival"
+              seeMoreUrl="/all-products"
+              products={products.slice(16, 28)}
+              className="mb-[60px]"
+          />
 
-      <ProductsAds
-        sectionHeight="164"
-        ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-4.png`]}
-        className="products-ads-section mb-[60px]"
-      />
-      <PopularSales
-        products={products.slice(16, 28)}
-        sectionTitle="Popular Sales"
-        seeMoreUrl="/all-products"
-        className="mb-[120px]"
-      />
-      <DiscountBanner />
-    </Layout>
+          <ProductsAds
+              sectionHeight="164"
+              ads={[
+                  `${
+                      import.meta.env.VITE_PUBLIC_URL
+                  }/src/assets/images/ads-4.png`,
+              ]}
+              className="products-ads-section mb-[60px]"
+          />
+          <PopularSales
+              products={products.slice(16, 28)}
+              sectionTitle="Popular Sales"
+              seeMoreUrl="/all-products"
+              className="mb-[120px]"
+          />
+          <DiscountBanner />
+      </Layout>
   );
 }
 
