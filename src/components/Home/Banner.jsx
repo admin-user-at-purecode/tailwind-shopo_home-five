0eb37949495c05717ec429e068051e66deb9a4d8
unknown <kaushik4759@gmail.com>
2024-03-14 11:15:00 +0530

    Update

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -47,26 +47,6 @@ export default function Banner({ className }) {
                         <h1 className="md:text-[60px] text-[40px]  font-bold text-qblack md:leading-[80px] leading-[40px] mb-[44px]">
                           Shop the Hottest Brands and Designs at Shop0
                         </h1>
-                        <a to="#" passhref="true">
-                          <div rel="noopener noreferrer">
-                            <div className="flex justify-center items-center bg-qh5-bwhite w-[160px] h-[52px] group rounded-lg hover:text-white relative transition-all duration-300 ease-in-out overflow-hidden cursor-pointer">
-                              <div className="flex space-x-1 items-center transition-all duration-300 ease-in-out relative z-10">
-                                <span className="text-sm font-600 tracking-wide leading-7 mr-2">
-                                  Shop Now
-                                </span>
-                                <span>
-                                  <Star />
-                                </span>
-                              </div>
-                              <div
-                                style={{
-                                  transition: `transform 0.25s ease-in-out`,
-                                }}
-                                className="w-full h-full bg-black absolute top-0 left-0 right-0 bottom-0 transform scale-x-0 group-hover:scale-x-100 origin-[center_left] group-hover:origin-[center_right]"
-                              ></div>
-                            </div>
-                          </div>
-                        </a>
                       </div>
                     </div>
                   </div>
@@ -90,26 +70,6 @@ export default function Banner({ className }) {
                         <h1 className="md:text-[60px] text-[40px]  font-bold text-qblack md:leading-[80px] leading-[40px] mb-[44px]">
                           Shop the Hottest Brands and Designs at Shop0
                         </h1>
-                        <a to="#" passhref="true">
-                          <div rel="noopener noreferrer">
-                            <div className="flex justify-center items-center bg-qh5-bwhite w-[160px] h-[52px] group rounded-lg hover:text-white relative transition-all duration-300 ease-in-out overflow-hidden cursor-pointer">
-                              <div className="flex space-x-1 items-center transition-all duration-300 ease-in-out relative z-10">
-                                <span className="text-sm font-600 tracking-wide leading-7 mr-2">
-                                  Shop Now
-                                </span>
-                                <span>
-                                  <Star />
-                                </span>
-                              </div>
-                              <div
-                                style={{
-                                  transition: `transform 0.25s ease-in-out`,
-                                }}
-                                className="w-full h-full bg-black absolute top-0 left-0 right-0 bottom-0 transform scale-x-0 group-hover:scale-x-100 origin-[center_left] group-hover:origin-[center_right]"
-                              ></div>
-                            </div>
-                          </div>
-                        </a>
                       </div>
                     </div>
                   </div>
@@ -133,26 +93,6 @@ export default function Banner({ className }) {
                         <h1 className="md:text-[60px] text-[40px]  font-bold text-qblack md:leading-[80px] leading-[40px] mb-[44px]">
                           Shop the Hottest Brands and Designs at Shop0
                         </h1>
-                        <a to="#" passhref="true">
-                          <div rel="noopener noreferrer">
-                            <div className="flex justify-center items-center bg-qh5-bwhite w-[160px] h-[52px] group rounded-lg hover:text-white relative transition-all duration-300 ease-in-out overflow-hidden cursor-pointer">
-                              <div className="flex space-x-1 items-center transition-all duration-300 ease-in-out relative z-10">
-                                <span className="text-sm font-600 tracking-wide leading-7 mr-2">
-                                  Shop Now
-                                </span>
-                                <span>
-                                  <Star />
-                                </span>
-                              </div>
-                              <div
-                                style={{
-                                  transition: `transform 0.25s ease-in-out`,
-                                }}
-                                className="w-full h-full bg-black absolute top-0 left-0 right-0 bottom-0 transform scale-x-0 group-hover:scale-x-100 origin-[center_left] group-hover:origin-[center_right]"
-                              ></div>
-                            </div>
-                          </div>
-                        </a>
                       </div>
                     </div>
                   </div>
